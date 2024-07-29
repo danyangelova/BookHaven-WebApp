282ce79d2172eb9125205f92009c67fe9776f978
danyangelova <daniela.ant.ang@gmail.com>
2024-07-29 16:33:51 +0300

    add routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,5 @@
+import { Routes, Route } from "react-router-dom"
+
 import Header from "./components/header/Header"
 import Home from "./components/home/Home"
 import AllBooks from "./components/all-books/AllBooks"
@@ -15,16 +17,16 @@ function App() {
       <Header />
 
       <main>
-        <Home />
-        {/* <AllBooks /> */}
-        {/* <BookDetails /> */}
 
-        {/* <Login /> */}
-        {/* <Register /> */}
-
-         {/* <PostBook /> */}
-         {/* <EditBook /> */}
-
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/allbooks" element={<AllBooks />} />
+          <Route path="/bookdetails" element={<BookDetails />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/post" element={<PostBook />} />
+          <Route path="/edit" element={<EditBook />} />
+        </Routes>
       </main>
     </>
   )
