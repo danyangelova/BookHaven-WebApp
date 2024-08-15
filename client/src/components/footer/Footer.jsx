8b1aee8047ff8abe53495a9a814806478dde954e
danyangelova <daniela.ant.ang@gmail.com>
2024-08-15 20:06:00 +0300

    fixed css issues

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,8 +1,8 @@
 export default function Footer() {
     return (
         <>
-            <div class="footerContainer">
-                <div class="footerContent">
+            <div className="footerContainer">
+                <div className="footerContent">
                     <p>&copy; 2024 BookHaven. All rights reserved | This template is made with ♥ by BookHaven.com</p>
                 </div>
             </div>
