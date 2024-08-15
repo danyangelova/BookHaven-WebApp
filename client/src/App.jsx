de1a3b5f9ac8b9f1c31c9c49bf3a89e7dfb433c8
danyangelova <daniela.ant.ang@gmail.com>
2024-08-15 19:50:15 +0300

    add footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import { PrivateRoute } from "./components/private-route/PrivateRoute"
 import Logout from "./components/logout/Logout"
 import UserBooks from "./components/user-books/UserBooks"
 import NotFound from "./components/not-found/NotFound"
+import Footer from "./components/footer/Footer"
 
 
 
@@ -41,6 +42,7 @@ function App() {
           </Route>
         </Routes>
       </main>
+      <Footer />
 
     </AuthContextProvider>
   )
---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return (
+        <>
+            <div class="footerContainer">
+                <div class="footerContent">
+                    <p>&copy; 2024 BookHaven. All rights reserved | This template is made with ♥ by BookHaven.com</p>
+                </div>
+            </div>
+        </>
+    )
+}
